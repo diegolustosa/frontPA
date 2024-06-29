b20bd891e8e226818853f8de15e54beae90807de
diegolustosa <diegolustosasantos@gmail.com>
2024-06-28 22:19:04 -0300

    subindo aula 08

---
+++ aula 08/script.js
@@ -0,0 +1,39 @@
+const body = window.document.getElementsByTagName("body")[0];
+
+console.log(window.document.createElement("h1"));
+
+const h1 = window.document.createElement("h1");
+
+const ol = window.document.createElement("ol");
+
+//PROCURAR CORRIGIR ESSE CÓDIGO QUANDO TIVER TEMPO ESTÁ UNDEFENID o for each listando com os append 
+const alunos = [
+    {nome: "Abobrinha",
+    ra: 122131},
+
+    {nome: "ronaldinho fenomeno",
+    ra: 123111},
+
+    {nome: "astolfo",
+    ra: 342421}
+]
+alunos.forEach((item) => {
+    const li = window.document.createElement("li");
+    li.innerHTML = item.li;
+    console.log(li);
+    ol.appendChild(li);
+});
+
+
+ol.innerHTML = "Start";
+li.innerHTML = "exec1";
+
+
+
+
+
+h1.innerHTML = "ola mundo!";
+
+body.appendChild(h1);
+body.appendChild(ol);
+
