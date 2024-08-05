c1089fc7ef663190302304b03a6cac0a97115efc
diegolustosa <diegolustosasantos@gmail.com>
2024-08-05 20:44:33 -0300

    Subindo a prova

---
+++ Prova/script.js
@@ -0,0 +1,9 @@
+const form = window.document.getElementsByTagName("form");
+const another = document.getElementsByTagName("input");
+function valida_form(another) {
+    if(onclick 'enviar') {
+        var receiver = input.value;
+        // NAO CONSIGO CHAMAR ESSA DESGRAÇA NO JAVASCRIPT E NEM FAZER A MARCAÇÃO CORRETA NO HTML
+        console.log();
+    }
+}
