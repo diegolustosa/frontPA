28a29e02bb8b9c541c56d6c759e7bef8f186f759
diegolustosa <diegolustosasantos@gmail.com>
2024-08-13 13:42:22 -0300

    prova ajuste

--- Prova/script.js
+++ Prova/script.js
@@ -1,9 +1,13 @@
-const form = window.document.getElementsByTagName("form");
-const another = document.getElementsByTagName("input");
-function valida_form(another) {
-    if(onclick 'enviar') {
-        var receiver = input.value;
-        // NAO CONSIGO CHAMAR ESSA DESGRAÇA NO JAVASCRIPT E NEM FAZER A MARCAÇÃO CORRETA NO HTML
-        console.log();
+function enviar() {
+    const nome_capturado_input = document.getElementById("nome");
+    const email_capturado_input = document.getElementById("email");
+    const mensagem_confirmacao = document.getElementById("mensagem_confirmacao");
+
+    if(nome_capturado_input.value !== "" && email_capturado_input.value !== "") {
+        mensagem_confirmacao.innerHTML = (`Muito obrigado(a), senhor(a) ${nome_capturado_input.value},
+        Acabamos de enviar um e-mail para ${email_capturado_input.value}`)
     }
+    else {
+    alert("Os campos não podem estar vazios.")
+}
 }
